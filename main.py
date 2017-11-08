619121b396efe9343e82a1fbc2ac2a6eb3712c84
Franz Vargas-Acuña <franzvargas91@gmail.com>
2017-11-08 02:51:10 +0000

    Add files via upload
    Progress on main program for lab display

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  untitled.py
+#  main.py
 #  
 #  Copyright 2017  <pi@raspberrypi>
 #  
@@ -27,24 +27,58 @@ import serial
 from datetime import datetime
 import Xbee
 
+dir_OD = 0x0E01
+dir_ORP = 0X0E04
+frame_OPT = 0x00
+frame_ID = 0x01
+reg_DATA = bytearray.fromhex("C1 3C")
+
+
 def main(args):
     return 0
 
 if __name__ == '__main__':
     import sys
+	
+	#Initializar xbee port
     xbee = XBee.XBee("/dev/ttyUSB0")
-    Msg = xbee.Receive()
-    if Msg:
-		year = datetime.now().year
-		month = datetime.now().month
-		day = datetime.now().day
-		hour = datetime.now().hour
-		minute = datetime.now().minute
-		second = datetime.now().second
-		
-		conn = sqlite3.connect('./db/PTAR_Residencial_Belen.db')
-		with conn:
-			cur=conn.cursor()
-			cur.execute("INSERT INTO mediciones values(?, ?, ?, ?, ?, ?, ?, ?);", (counter, year, month, day, hour, minute, second, DATA))
+	msg = 0
+	
+	#Send Data Request for OD and wait for the response
+	#requestData()
+	xbee.Send(req_DATA,dir_OD,frame_OPT,frame_ID)
+	while msg == 0
+		frame = xbee.Receive()
+		if frame != None:
+			msg = 1	
+			sleep(0.25)
+			year = datetime.now().year
+			month = datetime.now().month
+			day = datetime.now().day
+			hour = datetime.now().hour
+			minute = datetime.now().minute
+			second = datetime.now().second
+			data_OD = frame[7:-1]
+	
+	xbee.Send(req_DATA,dir_ORP,frame_OPT,frame_ID)
+	while msg == 0
+		frame = xbee.Receive()
+		if frame != None:
+			msg = 1	
+			sleep(0.25)
+			year = datetime.now().year
+			month = datetime.now().month
+			day = datetime.now().day
+			hour = datetime.now().hour
+			minute = datetime.now().minute
+			second = datetime.now().second
+			data_ORP = frame[7:-1]
+			
+
+	conn = sqlite3.connect('./db/PTAR_Residencial_Belen.db')
+	with conn:
+		cur=conn.cursor()
+		cur.execute("INSERT INTO mediciones values(?,?,?,?,?);", (id, date, time, data_OD, data_ORP))
+		#cur.execute("INSERT INTO mediciones values(?, ?, ?, ?, ?, ?, ?, ?);", (counter, year, month, day, hour, minute, second, data_OD))
 	
     sys.exit(main(sys.argv))
