8b4111f50cb1bac09badb232dfc5283287760ecc
Franz Vargas-Acuña <franzvargas91@gmail.com>
2017-12-15 11:24:10 -0600

    Progress on summary mail

--- send_summary.py
+++ send_summary.py
@@ -7,14 +7,24 @@ import pandas as pd
 #def send_summary():
 def main():
     conn = sqlite3.connect('./db/PTAR_Residencial_Belen.db')
-    cur _ conn.cursor()
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    #Fetching dates
     tomorrow =  datetime.strftime(datetime.now() + timedelta(1), '%Y-%m-%d')
     today = datetime.now().date()
-    cur.execute("select * from table where timestamp between ? and ?",(today,tomorrow)
-    todays_data = cur.fetchall()
-    print pd.read_sql_query(todays_data)
-    for row in todays_data:
-      print(row)
+
+    #Selecting all table data to extract column names
+    cur.execute("select * from temps;")
+    columns = cur.fetchone()
+
+    #Extracting today's data
+    cur.execute("select * from temps where timestamp between ? and ?;",(today,tomorrow))
+    today_data = cur.fetchall()
+
+    df=pd.DataFrame(todaysData)
+    df.columns = columns.keys()
+    print(df)
 
 if __name__ == '__main__':
     main()
