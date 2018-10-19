8db3639009278269a9da0b147b2ea3b29d7f187b
Franz Vargas-Acuña <franzvargas91@gmail.com>
2018-10-19 10:15:23 -0600

    Email management

---
+++ email_management.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""Programa encargado de agregar, editar o eliminar destinatarios
+
+Sistema: Desea agregar, editar o eliminar un destinatario
+1) agregar
+2) editar
+3) eliminar
+
+Agregar: abrir archivo en modo append, solicitar nombres del destinatario
+y luego el correo.
+
+Editar: solicitar nombre del destinatario a editar, leer archivo
+para identificar la línea a editar. Solicitar correo electronico
+corregido.
+
+Eliminar: solicitar nombre del destinatario a eliminar, leer archivo
+para identificar el destinatario a eliminar. Solicitar confirmación
+"""
